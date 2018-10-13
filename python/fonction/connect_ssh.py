10ea1fe8e470ae846964d30d096687eed2d7644c
derios <maury.frederic@gmail.com>
2018-10-13 23:19:32 +0200

    function
    initial commit

---
+++ python/fonction/connect_ssh.py
@@ -0,0 +1,6 @@
+import paramiko
+def nouvelle_co(ip,user,passwd):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    return client
